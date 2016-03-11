1c0b4617c505cba27d933c8cce9237fb29f879f5
Shawn Shojaie <shojaie.mac@gmail.com>
2016-03-11 01:32:11 -0800

    Add most of the support for orders. Need to add API_KEY pickup from local conf file.

--- stock_api.py
+++ stock_api.py
@@ -1,40 +1,61 @@
 import requests
 import json
 
-class Stock_Minion(object):
+class StockMinion(object):
 
-    def check_api():
+    def __init__(self, api_key = None):
+        self.session = requests.Session()
+        if(api_key):
+            header = {'X-Starfighter-Authorization' : api_key}
+            self.session.headers.update(header)
+
+    def check_api(self):
         
-        resp = requests.get(BASE_URL + 'heartbeat')
-        data = _process_response(resp.text, resp.status_code)
+        resp = self.session.get(BASE_URL + 'heartbeat')
+        data = StockMinion._process_response(resp.text, resp.status_code)
         return data
 
     
-    def check_venue(venue_symbol):
+    def check_venue(self, venue):
 
-        resp = requests.get(BASE_URL + 'venues/%s/heartbeat' % venue_symbol)
-        data = _process_response(resp.text, resp.status_code)    
+        resp = self.session.get(BASE_URL + 'venues/%s/heartbeat' % venue)
+        data = StockMinion._process_response(resp.text, resp.status_code)    
         return data
 
 
-    def get_stocks_on_venue(venue_symbol):
+    def get_stocks_on_venue(self, venue):
 
-        resp = requests.get(BASE_URL + 'venues/%s/stocks' % venue_symbol)
-        data = _process_response(resp.text, resp.status_code)
+        resp = self.session.get(BASE_URL + 'venues/%s/stocks' % venue)
+        data = StockMinion._process_response(resp.text, resp.status_code)
         return data
     
     
-    def get_orderbook(venue_symbol, stock_symbol):
+    def get_orderbook(self, venue, stock):
 
-        resp = requests.get(BASE_URL + 'venues/%s/stocks/%s' % (venue_symbol, stock_symbol))
-        data = _process_response(resp.text, resp.status_code)
+        resp = self.session.get(BASE_URL + 'venues/%s/stocks/%s' % (venue, stock))
+        data = StockMinion._process_response(resp.text, resp.status_code)
         return data
 
 
-    def place_order(account, venue_symbol, stock_symbol, price, qty, direction, order_type):
-        resp = requests.post(BASE_URL + '')
+    def place_order(self, **kwargs):
 
-    
+        mandatory = ['account', 'venue', 'stock', 'direction', 'orderType']
+        missing_args = [x for x in mandatory if x not in kwargs]
+        
+        if(missing_args):
+            raise ValueError("Missing '%s' arguments in function call" % (', '.join(missing_args)))
+        
+        # leave the dictionary packed
+        request_body = kwargs
+        resp = self.session.post(BASE_URL + 'venues/%s/stocks/%s/orders' % (kwargs['venue'], kwargs['stock']),
+                                 data = json.dumps(request_body))
+        data = StockMinion._process_response(resp.text, resp.status_code)
+        return data
+        
+
+
+        
+    @staticmethod
     def _process_json(json_obj):
 
         try:
@@ -42,10 +63,9 @@ class Stock_Minion(object):
         except ValueError as e:
             data = {}
             print(e)
-
         return data
 
-    
+    @staticmethod
     def _process_status(code):
 
         if(code != 200):
@@ -53,12 +73,11 @@ class Stock_Minion(object):
         else:
             pass
 
-    
+    @staticmethod
     def _process_response(json_obj, code):
-
-        data = _process_json(json_obj)
-        _process_status(code)
-
+        
+        data = StockMinion._process_json(json_obj)
+        StockMinion._process_status(code)
         return data
 
     
@@ -67,18 +86,23 @@ class Stock_Minion(object):
 if __name__ == '__main__':
 
     BASE_URL = "https://api.stockfighter.io/ob/api/"
-    TEST_EXCHANGE = "TESTEX"
+    TEST_VENUE = "TESTEX"
     TEST_STOCK    = "FOOBAR"
     TEST_ACCOUNT  = "EXB123456"
 
-    data = check_api()
+    instance = StockMinion()
+
+    data = instance.check_api()
     print(data)
 
-    data = check_venue(TEST_EXCHANGE)
+    data = instance.check_venue(TEST_VENUE)
     print(data)
 
-    data = get_stocks_on_venue(TEST_EXCHANGE)
+    data = instance.get_stocks_on_venue(TEST_VENUE)
     print(data)
 
-    data = get_orderbook(TEST_EXCHANGE, TEST_STOCK)
+    data = instance.get_orderbook(TEST_VENUE, TEST_STOCK)
+    print(data)
+
+    data = instance.place_order(account = TEST_ACCOUNT, venue = TEST_VENUE, stock = TEST_STOCK,  direction = "buy", orderType = "market")
     print(data)
