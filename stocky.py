4f84ec758981c1aa29f4d2ea52f6bfd2624f366d
Shawn Shojaie <shojaie.mac@gmail.com>
2016-03-30 00:02:40 -0700

    Transition one function to new calling API.

--- stocky.py
+++ stocky.py
@@ -3,37 +3,51 @@ import json
 
 BASE_URL = "https://api.stockfighter.io/ob/api/"
 
-class StockMinion(object):    
-    
-    def __init__(self, api_key = None):
-        self.session = requests.Session()
-        if(api_key):
-            header = {'X-Starfighter-Authorization' : api_key}
-            self.session.headers.update(header)
+class StockMinion(object):
 
+    
+    def __init__(self, api_key, account, venue, stock):
+        self.session = requests.Session()
+        header = {'X-Starfighter-Authorization' : api_key}
+        self.session.headers.update(header)
+        self.account = account
+        self.venue = venue
+        self.stock = stock
+        
+        
     def check_api(self):
         
-        resp = self.session.get(BASE_URL + 'heartbeat')
-        data = StockMinion._process_response(resp.text, resp.status_code)
+        #resp = self.session.get(BASE_URL + 'heartbeat')
+        #data = StockMinion._process_response(resp.text, resp.status_code)
+
+        data = StockMinion._call_api(self ,BASE_URL + 'heartbeat', 'get')
+
         return data
 
     
-    def check_venue(self, venue):
+    def check_venue(self):
 
+        venue = self.venue
+        
         resp = self.session.get(BASE_URL + 'venues/{0}/heartbeat'.format(venue))
         data = StockMinion._process_response(resp.text, resp.status_code)    
         return data
 
 
-    def get_stocks_on_venue(self, venue):
+    def get_stocks_on_venue(self):
 
+        venue = self.venue
+        
         resp = self.session.get(BASE_URL + 'venues/{0}/stocks'.format(venue))
         data = StockMinion._process_response(resp.text, resp.status_code)
         return data
     
     
-    def get_orderbook(self, venue, stock):
+    def get_orderbook(self):
 
+        venue = self.venue
+        stock = self.stock
+        
         resp = self.session.get(BASE_URL + 'venues/{0}/stocks/{1}'.format(venue, stock))
         data = StockMinion._process_response(resp.text, resp.status_code)
         return data
@@ -41,6 +55,10 @@ class StockMinion(object):
 
     # using kwargs here allows me to call this function with keywords or with a dict
     def place_order(self, **kwargs):
+
+        kwargs['account'] = self.account
+        kwargs['stock'] = self.stock
+        kwargs['venue'] = self.venue
 
         mandatory = ['account', 'venue', 'stock', 'qty', 'direction', 'orderType']
         missing_args = [x for x in mandatory if x not in kwargs]
@@ -56,24 +74,38 @@ class StockMinion(object):
         data = StockMinion._process_response(resp.text, resp.status_code)
         return data
         
-    def get_quote(self, venue, stock):
+    def get_quote(self):
 
+        venue = self.venue
+        stock = self.stock
+        
         resp = self.session.get(BASE_URL + 'venues/{0}/stocks/{1}/quote'.format(venue, stock))
         data = StockMinion._process_response(resp.text, resp.status_code)
         return data
 
-    def get_order_status(self, venue, stock, id):
+    def get_order_status(self, id):
+
+        venue = self.venue
+        stock = self.stock
+        
         resp = self.session.get(BASE_URL + 'venues/{0}/stocks/{1}/orders/{2}'.format(venue, stock, id))
         data = StockMinion._process_response(resp.text, resp.status_code)
         return data
 
-    def cancel_order(self, venue, stock, id):
+    def cancel_order(self, id):
+
+        venue = self.venue
+        stock = self.stock
+        
         resp = self.session.delete(BASE_URL + 'venues/{0}/stocks/{1}/orders/{2}'.format(venue, stock, id))
         data = StockMinion._process_response(resp.text, resp.status_code)
         return data
 
-    def get_all_orders(self, venue, account, stock = None):
+    def get_all_orders(self, stock = None):
 
+        venue = self.venue
+        account = self.account
+        
         if(stock):
             resp = self.session.get(BASE_URL + 'venues/{0}/accounts/{1}/stocks/{2}/orders'.format(venue, account, stock))
         else:
@@ -81,8 +113,11 @@ class StockMinion(object):
         data = StockMinion._process_response(resp.text, resp.status_code)
         return data
 
-    def _call_api(self, url, *args, **kwargs):
 
+    def _call_api(self, url, verb, *args, **kwargs):
+
+        func = getattr(self.session, verb)
+        resp = func(url, *args, **kwargs)
 
         data = StockMinion._process_response(resp.text, resp.status_code)
         return data
@@ -118,6 +153,8 @@ class StockMinion(object):
     
 if __name__ == '__main__':
 
+    import sys
+    
 
     TEST_VENUE = "TESTEX"
     TEST_STOCK    = "FOOBAR"
@@ -127,34 +164,37 @@ if __name__ == '__main__':
     with open('api.key', 'r') as secret_file:
         API_KEY = secret_file.readlines()[0].rstrip('\n')
 
-    instance = StockMinion(API_KEY)
+    instance = StockMinion(API_KEY, TEST_ACCOUNT, TEST_VENUE, TEST_STOCK)
 
     data = instance.check_api()
     print(data)
 
-    data = instance.check_venue(TEST_VENUE)
+    sys.exit()
+
+    data = instance.check_venue()
     print(data)
 
-    data = instance.get_stocks_on_venue(TEST_VENUE)
+    data = instance.get_stocks_on_venue()
     print(data)
 
-    data = instance.get_orderbook(TEST_VENUE, TEST_STOCK)
+    data = instance.get_orderbook()
     print(data)
 
-    data = instance.place_order(account = TEST_ACCOUNT, venue = TEST_VENUE, stock = TEST_STOCK, qty = 100, direction = "buy", orderType = "limit", price = 100)
+    data = instance.place_order(qty = 100, direction = "buy", orderType = "limit", price = 100)
     print(data)
 
-    data = instance.get_quote(TEST_VENUE, TEST_STOCK)
+    data = instance.get_quote()
     print(data)
 
-    data = instance.get_order_status(TEST_VENUE, TEST_STOCK, 1226)
+    data = instance.get_order_status(1226)
     print(data)
 
-    data = instance.cancel_order(TEST_VENUE, TEST_STOCK, 1226)
+    data = instance.cancel_order(1226)
     print(data)
 
-    data = instance.get_all_orders(TEST_VENUE, TEST_ACCOUNT)
+    data = instance.get_all_orders()
     print(data)
 
-    data = instance.get_all_orders(TEST_VENUE, TEST_ACCOUNT, TEST_STOCK)
+    data = instance.get_all_orders(TEST_STOCK)
     print(data)
+
